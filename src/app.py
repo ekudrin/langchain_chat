fd538cfa43bfd5afc0917387a6bd4643bcf01de8
Евгений Кудрин <evgeniiikudrin@gmail.com>
2025-09-19 14:13:55 +0300

    add ollama embeddings, chroma and webbaseloader

--- src/app.py
+++ src/app.py
@@ -1,19 +1,67 @@
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_ollama import OllamaEmbeddings, ChatOllama
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def get_response(user_input):
     return 'I don`t know how to do that.'
 
 
+def get_vectorstore_from_url(url):
+    loader = WebBaseLoader(url)
+    document = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+
+    vector_store = Chroma.from_documents(document_chunks, OllamaEmbeddings(model="llama3"))
+
+    return vector_store
+
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatOllama(model="llama3")
+
+    retriever = vector_store.as_retriever()
+
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name='chat_history'),
+        ('user', '{input}'),
+        ('user',
+         'Given the above conversation, generate a search query to look up in order to get information relevant to the converstaion.')
+    ])
+
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+    return retriever_chain
+
+
+def get_conversational_rag_chain(retriever_chain):
+    llm = ChatOllama(model="llama3")
+
+    prompt = ChatPromptTemplate.from_messages([
+        ('system', 'Answer the user`s question based on the below context:\n\n{context}'),
+        MessagesPlaceholder(variable_name='chat_history'),
+        ('user', '{input}'),
+    ])
+
+    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
+
+    create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+
 # config
 st.title('Chatbot')
-st.set_page_config(page_title='Chatbot',page_icon='🤖')
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = [
-        AIMessage(content='Hello, I am a bot. How can I help you?'),
-    ]
-
-
+st.set_page_config(page_title='Chatbot', page_icon='🤖')
 
 with st.sidebar:
     st.header('Settings')
@@ -23,7 +71,14 @@ if website_url is None or website_url == '':
     st.info('Please enter a website URL')
 
 else:
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content='Hello, I am a bot. How can I help you?'),
+        ]
+    vector_store = get_vectorstore_from_url(website_url)
 
+    retriever_chain = get_context_retriever_chain(vector_store)
 
     user_query = st.chat_input('Type your message...')
     if user_query is not None and user_query != '':
@@ -31,8 +86,14 @@ else:
         st.session_state.chat_history.append(HumanMessage(content=user_query))
         st.session_state.chat_history.append(AIMessage(content=response))
 
+        retrieved_documents = retriever_chain.invoke(
+            {
+                'chat_history': st.session_state.chat_history,
+                'input': user_query
+            }
+        )
 
-# convo
+    # convo
     for message in st.session_state.chat_history:
         if isinstance(message, AIMessage):
             with st.chat_message('AI'):
