43d635e6f69cc482c6025da7b294d5b66dc5dcdb
Евгений Кудрин <evgeniiikudrin@gmail.com>
2025-09-19 13:02:33 +0300

    add chat_history and convo

--- src/app.py
+++ src/app.py
@@ -1,11 +1,42 @@
-
 import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
 
-st.set_page_config(page_title='Chatbot')
+def get_response(user_input):
+    return 'I don`t know how to do that.'
 
+
+# config
 st.title('Chatbot')
+st.set_page_config(page_title='Chatbot',page_icon='🤖')
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content='Hello, I am a bot. How can I help you?'),
+    ]
+
+
 
 with st.sidebar:
     st.header('Settings')
     website_url = st.text_input('Website URL')
 
+if website_url is None or website_url == '':
+    st.info('Please enter a website URL')
+
+else:
+
+
+    user_query = st.chat_input('Type your message...')
+    if user_query is not None and user_query != '':
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+
+
+# convo
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message('AI'):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message('Human'):
+                st.write(message.content)
